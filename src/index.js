ced8886c61229ad210abc2cb6c84905b6e8519fd
Avani Joshi <avani.joshi@etecture.de>
2020-02-25 11:03:41 +0100

    initial settings and implementing auth0

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const {startDatabase} = require('./database/mongo');
+const {insertCity, getCities, deleteCity, updateCity} = require('./database/cities');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+
+// defining the Express app
+const app = express();
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all cities
+app.get('/', async (req, res) => {
+    res.send(await getCities());
+});
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://avanijoshi.auth0.com/.well-known/jwks.json`
+    }),
+    // Validate the audience and the issuer.
+    audience: 'https://air-quality',
+    issuer: `https://avanijoshi.auth0.com/`,
+    algorithms: ['RS256']
+});
+app.use(checkJwt);
+
+app.post('/', async (req, res) => {
+    const newCity= req.body;
+    await insertCity(newCity);
+    res.send({ message: 'New city inserted.' });
+  });
+  
+// endpoint to delete a City
+app.delete('/:id', async (req, res) => {
+    await deleteCity(req.params.id);
+    res.send({ message: 'City removed.' });
+});
+
+// endpoint to update a City
+app.put('/:id', async (req, res) => {
+    const updatedCity= req.body;
+    await updateCity(req.params.id, updatedCity);
+    res.send({ message: 'City updated.' });
+});
+
+startDatabase().then(async () => {
+    await insertCity({city: 'Hello, now from the in-memory database!'});
+  
+    // starting the server
+    app.listen(4000, () => {
+        console.log('listening on port: 4000');
+    });
+});

0c29e5cff6739494caa7810f9ed59c67c7b63e01
Avani Joshi <avani.joshi@etecture.de>
2020-03-02 21:37:36 +0100

    refactoring

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+export const COLLECTION_NAME = "AirQualityInCities";
+export const PORT = 4000;
+export const DB_USERNAME = "AirQualityIndexDB";
+export const DB_PASSWORD = "AirQualityIndexDB123";
+
+export const JWKS_URI = "https://avanijoshi.auth0.com/.well-known/jwks.json";
+export const AUDIENCE = "https://air-quality";
+export const ISSUER = "https://avanijoshi.auth0.com/";
+export const ALGORITHMS = ["RS256"];
